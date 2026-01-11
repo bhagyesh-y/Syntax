178551885383f3dfe33bc0d3343811a5dac14e97
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-11 10:46:08 +0530

    added cateogory model and configured settings for  media

--- Syntax/settings.py
+++ Syntax/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp'
+    'testapp',
+    'blogs'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'Syntax/static',
 ]
+
+MEDIA_URL = '/media/'
+MEDAI_ROOT = BASE_DIR /'media'
