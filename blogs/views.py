eac2e10fb9be56d15d6d2318914fd02cb84cf201
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-12 01:09:24 +0530

    added urls and view for posts by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Category,Blog
 
@@ -5,10 +6,21 @@ from .models import Category,Blog
 def Home(request):
     categories = Category.objects.all()
     featured_posts=Blog.objects.filter(is_featured=True).order_by('updated_at')
-    posts = Blog.objects.filter(is_featured = False,status='Published')
+    posts = Blog.objects.filter(is_featured=True,status='Published')
+    print(posts)
     context={
         'categories':categories,
         'featured_posts':featured_posts,
         'posts':posts,
     }
     return render(request,'home.html',context)
+
+
+
+def posts_by_category(request,category_id):
+    # Fetch the post that belongs to the category with the id category_id
+    posts = Blog.objects.filter(status="Published",category=category_id)
+    context={
+        'posts':posts
+    }
+    return render(request,'posts_by_category.html',context)
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,1 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
 
-# def Home(request):
-#     return render(request,'home.html')
