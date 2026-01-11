40dbc55e3c0821b8b15571cf21b0677cace158c3
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-11 11:11:53 +0530

    added blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50 , unique=True)
@@ -11,3 +12,22 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
     
+STATUS_CHOICES = (
+    (0,"Draft"),
+    (1,"Published")
+)
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150,unique=True,blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=1000)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__ (self):
+        return self.title
