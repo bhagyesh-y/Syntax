1b3c9356b6755f207077a6b4701b6c74eb91dafa
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-12 16:01:54 +0530

    added single blog page

--- Syntax/urls.py
+++ Syntax/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from blogs import views
+from blogs import views 
 from django.conf.urls.static import static
 from django.conf import settings
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home,name='home'),
     path('category/',include('blogs.urls')),
+    path('<slug:slug>/', views.blogs,name='blogs' ),
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
