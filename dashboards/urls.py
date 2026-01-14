928a8558223bbef5a46f711f29e47cd40beb2d17
bhagyesh508 <bhagyeshyadav29@gmail.com>
2026-01-14 13:23:17 +0530

    added functionality for adding new blogs

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
+    # Category CRUD Operations
     path('categories/',views.categories,name='categories'),
     path('category/add/',views.add_category,name='add_category'),
     path('category/edit/<int:pk>/',views.edit_category,name='edit_category'),
     path('category/delete/<int:pk>/',views.delete_category,name='delete_category'),
+    # Blog Posts CRUD
+    path('posts/', views.posts,name='posts'),
+    path('posts/add/',views.add_post,name='add_post'),
 ]
 
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Category,Blog
 from django.contrib.auth.decorators import login_required
-from dashboards.forms import CategoryForm
+from dashboards.forms import CategoryForm , BlogPostForm
+from django.template.defaultfilters import slugify
 
 # view function for dashboard
 @login_required(login_url='login')
@@ -52,3 +53,35 @@ def delete_category(request,pk):
     category = Category.objects.get(pk=pk)
     category.delete()
     return redirect ('categories')
+
+
+
+""" functions for blogs """
+# view function for posts 
+def posts(request):
+    post=Blog.objects.all()
+    context={
+        'posts':post
+    }
+    return render(request,'dashboard/posts.html' , context)
+
+# view function for adding new post
+def add_post(request):
+    if request.method =='POST':
+        form = BlogPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False) # temporarily saving the form 
+            post.author=request.user           
+            post.save()
+            title = form.cleaned_data['title']
+            post.slug=slugify(title) + '-'+str(post.id)
+            post.save()
+            return redirect('posts')
+        else:
+            print("form is not valid")
+            print(form.errors)
+    form = BlogPostForm()
+    context = {
+            'form':form,
+        }
+    return render (request,'dashboard/add_post.html', context)
